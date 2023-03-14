0774ee6cdea8bba7ce2443e9bc269e1ab0732dba
Brandon <17894275+brandoncorry@users.noreply.github.com>
2023-03-13 17:52:12 -0700

    Updated Futures Calc form to have a similar message.

--- TdInterface/Forms/FurtureCalcForm.Designer.cs
+++ TdInterface/Forms/FurtureCalcForm.Designer.cs
@@ -46,169 +46,185 @@
             this.label7 = new System.Windows.Forms.Label();
             this.txtStop = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtSymbol
             // 
-            this.txtSymbol.Location = new System.Drawing.Point(174, 91);
+            this.txtSymbol.Location = new System.Drawing.Point(12, 31);
             this.txtSymbol.Name = "txtSymbol";
-            this.txtSymbol.Size = new System.Drawing.Size(125, 27);
+            this.txtSymbol.Size = new System.Drawing.Size(125, 26);
             this.txtSymbol.TabIndex = 1;
             this.txtSymbol.Leave += new System.EventHandler(this.txtSymbol_Leave);
             // 
             // lblSymbol
             // 
             this.lblSymbol.AutoSize = true;
-            this.lblSymbol.Location = new System.Drawing.Point(95, 96);
+            this.lblSymbol.Location = new System.Drawing.Point(12, 9);
             this.lblSymbol.Name = "lblSymbol";
-            this.lblSymbol.Size = new System.Drawing.Size(59, 20);
+            this.lblSymbol.Size = new System.Drawing.Size(54, 19);
             this.lblSymbol.TabIndex = 1;
             this.lblSymbol.Text = "Symbol";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(95, 135);
+            this.label1.Location = new System.Drawing.Point(12, 72);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.Size = new System.Drawing.Size(69, 19);
             this.label1.TabIndex = 3;
             this.label1.Text = "Tick Value";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(95, 246);
+            this.label2.Location = new System.Drawing.Point(158, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.Size = new System.Drawing.Size(37, 19);
             this.label2.TabIndex = 5;
             this.label2.Text = "Stop";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(574, 72);
+            this.label6.Location = new System.Drawing.Point(545, 9);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(32, 20);
+            this.label6.Size = new System.Drawing.Size(31, 19);
             this.label6.TabIndex = 31;
             this.label6.Text = "Ask";
             // 
             // txtAsk
             // 
-            this.txtAsk.Location = new System.Drawing.Point(574, 96);
+            this.txtAsk.Location = new System.Drawing.Point(539, 32);
             this.txtAsk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtAsk.Name = "txtAsk";
             this.txtAsk.ReadOnly = true;
-            this.txtAsk.Size = new System.Drawing.Size(114, 27);
+            this.txtAsk.Size = new System.Drawing.Size(114, 26);
             this.txtAsk.TabIndex = 30;
             this.txtAsk.TabStop = false;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(466, 72);
+            this.label5.Location = new System.Drawing.Point(429, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 20);
+            this.label5.Size = new System.Drawing.Size(28, 19);
             this.label5.TabIndex = 29;
             this.label5.Text = "Bid";
             // 
             // txtBid
             // 
-            this.txtBid.Location = new System.Drawing.Point(464, 96);
+            this.txtBid.Location = new System.Drawing.Point(429, 32);
             this.txtBid.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtBid.Name = "txtBid";
             this.txtBid.ReadOnly = true;
-            this.txtBid.Size = new System.Drawing.Size(104, 27);
+            this.txtBid.Size = new System.Drawing.Size(104, 26);
             this.txtBid.TabIndex = 28;
             this.txtBid.TabStop = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(361, 72);
+            this.label4.Location = new System.Drawing.Point(326, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.Size = new System.Drawing.Size(34, 19);
             this.label4.TabIndex = 27;
             this.label4.Text = "Last";
             // 
             // txtLastPrice
             // 
-            this.txtLastPrice.Location = new System.Drawing.Point(361, 96);
+            this.txtLastPrice.Location = new System.Drawing.Point(326, 32);
             this.txtLastPrice.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtLastPrice.Name = "txtLastPrice";
             this.txtLastPrice.ReadOnly = true;
-            this.txtLastPrice.Size = new System.Drawing.Size(97, 27);
+            this.txtLastPrice.Size = new System.Drawing.Size(97, 26);
             this.txtLastPrice.TabIndex = 26;
             this.txtLastPrice.TabStop = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(95, 55);
+            this.label3.Location = new System.Drawing.Point(158, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.Size = new System.Drawing.Size(33, 19);
             this.label3.TabIndex = 32;
             this.label3.Text = "Risk";
             // 
             // txtShares
             // 
-            this.txtShares.Location = new System.Drawing.Point(298, 338);
+            this.txtShares.Location = new System.Drawing.Point(326, 94);
             this.txtShares.Name = "txtShares";
             this.txtShares.ReadOnly = true;
-            this.txtShares.Size = new System.Drawing.Size(125, 27);
+            this.txtShares.Size = new System.Drawing.Size(125, 26);
             this.txtShares.TabIndex = 34;
             this.txtShares.TabStop = false;
             // 
             // txtRisk
             // 
-            this.txtRisk.Location = new System.Drawing.Point(174, 48);
+            this.txtRisk.Location = new System.Drawing.Point(158, 31);
             this.txtRisk.Name = "txtRisk";
-            this.txtRisk.Size = new System.Drawing.Size(125, 27);
+            this.txtRisk.Size = new System.Drawing.Size(125, 26);
             this.txtRisk.TabIndex = 0;
             // 
             // txtTickValue
             // 
-            this.txtTickValue.Location = new System.Drawing.Point(174, 135);
+            this.txtTickValue.Location = new System.Drawing.Point(12, 94);
             this.txtTickValue.Name = "txtTickValue";
-            this.txtTickValue.Size = new System.Drawing.Size(125, 27);
+            this.txtTickValue.Size = new System.Drawing.Size(125, 26);
             this.txtTickValue.TabIndex = 3;
             // 
             // txtTicksPerPoint
             // 
-            this.txtTicksPerPoint.Location = new System.Drawing.Point(222, 178);
+            this.txtTicksPerPoint.Location = new System.Drawing.Point(12, 154);
             this.txtTicksPerPoint.Name = "txtTicksPerPoint";
-            this.txtTicksPerPoint.Size = new System.Drawing.Size(125, 27);
+            this.txtTicksPerPoint.Size = new System.Drawing.Size(125, 26);
             this.txtTicksPerPoint.TabIndex = 4;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(95, 181);
+            this.label7.Location = new System.Drawing.Point(12, 132);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(102, 20);
+            this.label7.Size = new System.Drawing.Size(96, 19);
             this.label7.TabIndex = 37;
             this.label7.Text = "Ticks Per Point";
             // 
             // txtStop
             // 
-            this.txtStop.Location = new System.Drawing.Point(174, 239);
+            this.txtStop.Location = new System.Drawing.Point(158, 94);
             this.txtStop.Name = "txtStop";
-            this.txtStop.Size = new System.Drawing.Size(125, 27);
+            this.txtStop.Size = new System.Drawing.Size(125, 26);
             this.txtStop.TabIndex = 2;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(332, 293);
+            this.label8.Location = new System.Drawing.Point(326, 72);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(63, 20);
+            this.label8.Size = new System.Drawing.Size(57, 19);
             this.label8.TabIndex = 38;
             this.label8.Text = "SHARES";
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox1.Location = new System.Drawing.Point(12, 197);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(641, 62);
+            this.textBox1.TabIndex = 39;
+            this.textBox1.Text = "At this time, there is NO support for futures trading in the application.\r\nIt is " +
+    "on our list of features to add.";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // FurtureCalcForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(669, 257);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.txtStop);
             this.Controls.Add(this.txtTicksPerPoint);
@@ -228,7 +244,7 @@
             this.Controls.Add(this.lblSymbol);
             this.Controls.Add(this.txtSymbol);
             this.Name = "FurtureCalcForm";
-            this.Text = "FurtureCalcForm";
+            this.Text = "Futures Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -254,5 +270,6 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtStop;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
